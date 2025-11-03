aba242b75c4577c92d0a726e020816e7afcc170d
vanrobbins <vanr0423@gmail.com>
2025-11-03 11:23:19 -0500

    feat: integrate Firebase authentication and remove unused files
    - Added Firebase as a dependency for authentication features.
    - Implemented user sign-in, sign-up, and Google sign-in functionality in main.js.
    - Removed unused counter.js and javascript.svg files to clean up the project.
    - Updated style.css to reflect the new layout for the login form.

---
+++ week10/firebase-auth/README.md
@@ -0,0 +1,2 @@
+.exp.env needs to be changed to .local.env or .env
+Also needs to be changed with a firebase api key
--- week10/firebase-auth/src/main.js
+++ week10/firebase-auth/src/main.js
@@ -1,24 +1,80 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Import the functions you need from the SDKs you need
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import { initializeApp } from "firebase/app";
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	GoogleAuthProvider,
+	signInWithPopup,
+	signOut,
+	onAuthStateChanged,
+} from "firebase/auth";
 
-setupCounter(document.querySelector('#counter'))
+//Firebase configuration
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_PUBLIC_API_KEY,
+
+	authDomain: import.meta.env.VITE_PUBLIC_AUTH_DOMAIN,
+
+	projectId: import.meta.env.VITE_PUBLIC_PROJECT_ID,
+
+	storageBucket: import.meta.env.VITE_PUBLIC_STORAGE_BUCKET,
+
+	messagingSenderId: import.meta.env.VITE_PUBLIC_MSG_SENDER_ID,
+
+	appId: import.meta.env.VITE_PUBLIC_APP_ID,
+};
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+onAuthStateChanged(auth, (user) => {
+	if (user) {
+		console.log("User:", user.email, "is signed in");
+		return;
+	}
+	console.log("No user signed in");
+});
+document.getElementById("login-btn").onclick = () => {
+	let email = document.getElementById("email").value;
+	let password = document.getElementById("password").value;
+	signInWithEmailAndPassword(auth, email, password)
+		.then(() => {
+			console.log("signed in:", email);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
+
+document.getElementById("signup-btn").onclick = () => {
+	let email = document.getElementById("email").value;
+	let password = document.getElementById("password").value;
+	createUserWithEmailAndPassword(auth, email, password)
+		.then(() => {
+			console.log("signed in:", email);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
+document.getElementById("googleSignin").onclick = () => {
+	const provider = new GoogleAuthProvider();
+	signInWithPopup(auth, provider)
+		.then((result) => {
+			console.log("user:", result.user);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
+document.getElementById("signOut").onclick = () => {
+	signOut(auth)
+		.then(() => {
+			console.log("signed out");
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
